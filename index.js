258a7e0350fe0628c8f1cdafd8731699df49fba3
Ashley George <geoashley@gmail.com>
2022-03-13 22:33:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var server_camera_port = 9000;
 var server_port = 65432;
 var server_addr = "192.168.86.206";   // the IP address of your Raspberry PI
 const piClient = net.createConnection({ port: server_port, host: server_addr })
-const piCameraClient = net.createConnection({ port: server_camera_port, host: server_addr })
+
 
 function register(){
     console.log("to register ", piClient)
@@ -31,71 +31,6 @@ function register(){
     registerCamera()
 }
 
-function registerCamera(){
-    console.log("to register ", piClient)
-    piCameraClient.on('connect', () => {
-        console.log('connected to pi camera server!');
-    });
-    // get the data from the server
-    piCameraClient.on('data', (data) => {
-        console.log("image data ",data);
-        //document.getElementById("bluetooth").innerHTML = data;
-
-
-        //imgElement = document.getElementById("pifeed")
-
-
-        //img.src = 'data:image/jpeg;base64,' + data.buffer;
-        //var base64String = Buffer.from(data.buffer, 'hex').toString('base64')
-
-        // var img = new Image();
-        // img.src = 'data:image/jpeg;base64,' + base64String;
-        // imgElement.drawImage(img, 0, 0);
-
-        //imgElement.src = 'data:image/jpeg;base64,' + data;
-        
-
-
-    });
-
-    piCameraClient.on('end', () => {
-        console.log('disconnected from pi camera server');
-    });
-}
-
-function hexToBase64(str) {
-    return btoa(String.fromCharCode.apply(null, str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" ")));
-}
-function client(value){
-    
-
-    var input = document.getElementById("message").value;
-
-    const client = net.createConnection({ port: server_port, host: server_addr }, () => {
-        // 'connect' listener.
-        console.log('connected to server!');
-        // send the message
-        //client.write(`${value}`);
-        client.write(value);
-    });
-    
-    // get the data from the server
-    client.on('data', (data) => {
-        //document.getElementById("bluetooth").innerHTML = data;
-        document.getElementById("bluetooth").innerHTML = data;
-        
-        console.log(data.toString());
- 
-        // client.end();
-        // client.destroy();
-    });
-
-    client.on('end', () => {
-        console.log('disconnected from server');
-    });
-
-
-}
 
 // for detecting which key is been pressed w,a,s,d
 function updateKey(e) {
